1ae8faf09ec1207797d6096a19399d5e01d36eed
AlanCT94 <caciqueta94@gmail.com>
2022-10-19 17:14:26 +0200

    oxygen

--- R/methods_BL.R
+++ R/methods_BL.R
@@ -12,13 +12,27 @@
 #'           s<-ridgereg(formula=Petal.Length~Species, data=iris, 2)
 #'           print(s)
 print.ridgereg <- function(x, ...){
+  cat("Call:")
+  print(x$call)
   print(x$coefficients)
+}
+
+#' Predict
+#'
+#' @param x Refers to the object return by ridge regression
+#' @param ... argument to be passed to methods
+#'
+#' @return fitted values
+#' @export
+#'
+#' @examples data(iris)
+#'           s<-ridgereg(formula=Petal.Length~Species, data=iris, 2)
+#'           predict(s)
+predict.ridgereg <- function(x, ...){
+  print(x$fitted_values)
 }
 
 coef.ridgereg <- function(x, ...){
   print(x$coefficients)
 }
 
-predict.ridgereg <- function(x, ...){
-  print(x$fitted_values)
-}
