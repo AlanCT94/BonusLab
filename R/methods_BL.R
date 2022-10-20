66cf45e684e16f20400bbf5a11c951b1804f34e8
AlanCT94 <caciqueta94@gmail.com>
2022-10-20 10:59:34 +0200

    Roxygen

--- R/methods_BL.R
+++ R/methods_BL.R
@@ -12,14 +12,24 @@
 #'           s<-ridgereg(formula=Petal.Length~Species, data=iris, 2)
 #'           print(s)
 print.ridgereg <- function(x, ...){
+  cat("", sep="\n\n")
+
   cat("Call:")
+
+  cat("", sep="\n\n")
+
   print(x$call)
+
+  cat("", sep="\n\n")
+
+  cat("Coefficients:")
+  cat("", sep="\n\n")
   print(x$coefficients)
 }
 
 #' Predict
 #'
-#' @param x Refers to the object return by ridge regression
+#' @param object Refers to the object return by ridge regression
 #' @param ... argument to be passed to methods
 #'
 #' @return fitted values
@@ -28,11 +38,22 @@ print.ridgereg <- function(x, ...){
 #' @examples data(iris)
 #'           s<-ridgereg(formula=Petal.Length~Species, data=iris, 2)
 #'           predict(s)
-predict.ridgereg <- function(x, ...){
-  print(x$fitted_values)
+predict.ridgereg <- function(object, ...){
+  print(object$fitted_values)
 }
 
-coef.ridgereg <- function(x, ...){
-  print(x$coefficients)
+#' B coefficients
+#'
+#' @param object Refers to the object return by ridge regression
+#' @param ... argument to be passed to methods
+#'
+#' @return Coefficients
+#' @export
+#'
+#' @examples data(iris)
+#' s<-ridgereg(formula=Petal.Length~Species, data=iris, 2)
+#' coef(s)
+coef.ridgereg <- function(object, ...){
+  print(object$coefficients)
 }
 
